65ac279c0aa878aa8d889ed5a5961b92608bd0f3
James Fordham <jimpfordham@gmail.com>
2017-07-15 11:22:57 +1000

    My first Laravel App

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller {
+    public function about() {
+        return view('pages/about');
+    }
+}
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ashton is a boofhead</title>
+</head>
+<body>
+<h1>Ashton is a Boofhead!</h1>
+<p>B - O - O - F -> Boofhead</p>
+</body>
+
+</html>
